c3f26510c9e0e75a913d18053b58c05fc5962e83
Hung Tran Manh <hungtranmanh.90@gmail.com>
2016-01-30 22:16:24 +0700

    test

--- app/src/main/java/cocomo/com/cocomoprototype/MainActivity.java
+++ app/src/main/java/cocomo/com/cocomoprototype/MainActivity.java
@@ -11,45 +11,47 @@ import android.widget.TextView;
 
 /**
  * @author Adil Soomro
- *
  */
 @SuppressWarnings("deprecation")
 public class MainActivity extends TabActivity {
-	TabHost tabHost;
-	/** Called when the activity is first created. */
-	
-	
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		tabHost = getTabHost();
-		setTabs();
-	}
-	private void setTabs()
-	{
-		addTab("Home", R.drawable.tab_home, MapsActivity.class);
-		addTab("Search", R.drawable.tab_search, OtherActivity.class);
-		addTab("Fake", R.drawable.tab_search, OtherActivity.class);
-		addTab("Home", R.drawable.tab_home, OtherActivity.class);
-		addTab("Search", R.drawable.tab_search, OtherActivity.class);
-	}
-	private void addTab(String labelId, int drawableId, Class<?> c)
-	{
-		Intent intent = new Intent(this, c);
-		TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);
-		
-		View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
-		TextView title = (TextView) tabIndicator.findViewById(R.id.title);
-		title.setText(labelId);
-		ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
-		icon.setImageResource(drawableId);		
-		spec.setIndicator(tabIndicator);
-		spec.setContent(intent);
-		tabHost.addTab(spec);
-	}
-	public void openCameraActivity(View b)
-	{
-		Intent intent = new Intent(this, MapsActivity.class);
-		startActivity(intent);
-	}
+    TabHost tabHost;
+
+    /**
+     * Called when the activity is first created.
+     */
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        tabHost = getTabHost();
+        setTabs();
+    }
+
+    private void setTabs() {
+        addTab("Home", R.drawable.tab_home, MapsActivity.class);
+        addTab("Search", R.drawable.tab_search, OtherActivity.class);
+        addTab("Fake", R.drawable.tab_search, OtherActivity.class);
+        addTab("Home", R.drawable.tab_home, OtherActivity.class);
+        addTab("Search", R.drawable.tab_search, OtherActivity.class);
+    }
+
+    private void addTab(String labelId, int drawableId, Class<?> c) {
+        Intent intent = new Intent(this, c);
+        TabHost.TabSpec spec = tabHost.newTabSpec("tab" + labelId);
+
+        View tabIndicator = LayoutInflater.from(this).inflate(R.layout.tab_indicator, getTabWidget(), false);
+        TextView title = (TextView) tabIndicator.findViewById(R.id.title);
+        title.setText(labelId);
+        ImageView icon = (ImageView) tabIndicator.findViewById(R.id.icon);
+        icon.setImageResource(drawableId);
+        spec.setIndicator(tabIndicator);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+    }
+
+    public void openCameraActivity(View b) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
