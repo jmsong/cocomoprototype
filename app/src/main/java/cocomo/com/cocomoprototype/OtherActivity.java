f18321ec784979e639d98bdee0dc5b626c5f3a77
Tran Minh Tung <minhtunggt@gmail.com>
2016-01-30 21:45:38 +0700

    Initial commit

---
+++ app/src/main/java/cocomo/com/cocomoprototype/OtherActivity.java
@@ -0,0 +1,16 @@
+package cocomo.com.cocomoprototype;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Adil Soomro
+ *
+ */
+public class OtherActivity extends Activity {
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);      
+    }
+}
